241d328647a686cc4196be5618918f0f3d6d648a
tatyderb <tatyderb@gmail.com>
2022-09-10 00:21:08 +0300

    черное окно появляется и пропадает

--- space_invaders/space_invaders.py
+++ space_invaders/space_invaders.py
@@ -3,3 +3,10 @@ print(pygame.version.ver)
 
 pygame.init()
 
+# Размеры окна
+display_width = 800
+display_height = 600
+display_size = (display_width, display_height)
+
+# создаем окно
+pygame.display.set_mode(display_size)

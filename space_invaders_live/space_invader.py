82163fbd88fc2d7ba9d5baab8f2720b66fc3c836
tatyderb <tatyderb@gmail.com>
2022-09-10 10:43:40 +0300

    создаем окно, закрываем по Х

--- space_invaders_live/space_invader.py
+++ space_invaders_live/space_invader.py
@@ -1,1 +1,20 @@
 import pygame
+
+pygame.init()
+
+# размеры окна
+display_width = 800
+display_height = 600
+display_size = (display_width, display_height)
+# print(f'{display_width=} {display_height=} {display_size=}')
+
+# создаем окно
+pygame.display.set_mode(display_size)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()

d82c4a3c5be810f9def0d3e2dfbc57a9b1b67096
tatyderb <tatyderb@gmail.com>
2022-09-11 14:21:36 +0300

    add bullet by mouse click

--- space_invaders_live/space_invaders_stepik.py
+++ space_invaders_live/space_invaders_stepik.py
@@ -5,6 +5,7 @@ pygame.init()
 # загружаем изображения
 icon_img = pygame.image.load('resources/img/ufo.png')
 player_img = pygame.image.load('resources/img/player.png')
+bullet_img = pygame.image.load('resources/img/bullet.png')
 
 # размеры окна
 display_width = 800
@@ -26,6 +27,14 @@ player_y = display_height - player_height - player_gap
 player_speed = 1
 player_dx = player_speed
 
+# пуля
+bullet_alive = False
+bullet_width = bullet_img.get_width()
+bullet_height = bullet_img.get_height()
+bullet_x, bullet_y, bullet_dy = 0, 0, 0
+bullet_speed = 5
+
+
 # изменение положения объектов
 def player_update():
     """Обновляет позицию игрока."""
@@ -39,6 +48,19 @@ def player_update():
     if player_x + player_width > display_width:
         player_x = display_width - player_width
 
+def bullet_create():
+    """Возвращает координаты пули"""
+    x = player_x + player_width / 2 - bullet_width / 2
+    y = player_y - bullet_height
+    dy = -bullet_speed
+    return x, y, dy
+
+def bullet_update():
+    global bullet_y
+    if bullet_alive:
+        bullet_y += bullet_dy
+
+
 def model_update():
     player_update()
 
@@ -48,6 +70,9 @@ def display_redraw():
     # рисуем на экране
     display.fill('black', (0, 0, display_width, display_height))
     display.blit(player_img, (player_x, player_y))
+    if bullet_alive:
+        display.blit(bullet_img, (bullet_x, bullet_y))
+
     pygame.display.update()
 
 # обработка событий
@@ -66,6 +91,15 @@ def event_player(event):
                          pygame.K_a, pygame.K_d):
             player_dx = 0
 
+def event_bullet(event):
+    """Стреляет по нажатию левой кнопки мыши."""
+    global bullet_alive, bullet_x, bullet_y, bullet_dy
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        key = pygame.mouse.get_pressed()
+        print(f'{key=} {bullet_alive=}')
+        if key[0] and not bullet_alive:
+            bullet_x, bullet_y, bullet_dy = bullet_create()
+            bullet_alive = True
 
 def event_close_application(event):
     return event.type == pygame.QUIT
@@ -75,6 +109,7 @@ def event_process():
     """Обрабатывает события клавиатуры и мыши, возвращает False, если приложение хотят закрыть."""
     for event in pygame.event.get():
         event_player(event)
+        event_bullet(event)
         if event_close_application(event):
             return False
     return True

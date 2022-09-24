33e53910eea36ec805b8d158ead886def9912ff8
tatyderb <tatyderb@gmail.com>
2022-09-24 11:16:35 +0300

    Player перенесен из нее ООП версии

--- space_invaders_live/space_invaders_live_oop.py
+++ space_invaders_live/space_invaders_live_oop.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 RSC = {
     'title': 'Space Invaders',
     'img': {
@@ -15,18 +14,57 @@ RSC = {
 }
 
 
-class Game:
-    def __init__(self, size):
-        pass
+class Player:
+    YGAP = 10
+    SPEED = 0.5
+
+    def __init__(self, display_size):
+        self.bound_size = self.bound_width, self.bound_height = display_size
+        self.img = pygame.image.load(RSC['img']['player'])
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+        self.x = self.bound_width // 2 - self.width // 2
+        self.y = self.bound_height - self.height - self.YGAP
+        self.dx = 0
 
     def model_update(self):
-        pass
+        self.x += self.dx
+        # не дадим игроку выходить за пределы окна
+        if self.x < 0:
+            self.x = 0
+        if self.x > self.bound_width - self.width:
+            self.x = self.bound_width - self.width
 
     def redraw(self, display):
-        pass
+        display.blit(self.img, (self.x, self.y))
 
-    def event_process(self):
-        pass
+    def event_process(self, event):
+        # нажали клавишу
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.dx = -self.SPEED
+            elif event.key == pygame.K_RIGHT:
+                self.dx = self.SPEED
+        # отпустили клавишу
+        if event.type == pygame.KEYUP:
+            self.dx = 0
+
+
+class Game:
+    def __init__(self, size):
+        self.player = Player(size)
+
+    def model_update(self):
+        self.player.model_update()
+
+    def redraw(self, display, size):
+        width, height = size
+        display.fill((0, 0, 0), (0, 0, width, height))
+        self.player.redraw(display)
+        pygame.display.update()
+
+    def event_process(self, event):
+        self.player.event_process(event)
 
 
 class Application:
@@ -46,11 +84,11 @@ class Application:
         game = Game(self.size)
         while running:
             game.model_update()
-            game.redraw(self.display)
+            game.redraw(self.display, self.size)
             for event in pygame.event.get():
                 if self.event_close_application(event):
                     running = False
-                game.event_process()
+                game.event_process(event)
 
     def event_close_application(self, event):
         return event.type == pygame.QUIT

a93605347c9f0af909c270185ce7d4318ffdbd77
tatyderb <tatyderb@gmail.com>
2022-09-24 20:37:36 +0300

    Player - добавили игрока

--- space_invaders_live/space_invaders_stepik_oop.py
+++ space_invaders_live/space_invaders_stepik_oop.py
@@ -21,18 +21,64 @@ RSC = {
 }
 
 
-class Game:
+class Player:
+    YGAP = 10           # расстояние между игроком и низом экрана
+    SPEED = 0.5         # скорость игрока при движении
+
     def __init__(self, display_size):
-        pass
+        self.bound_size = self.bound_width, self.bound_height = display_size
+        self.img = pygame.image.load(RSC['img']['player'])
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+        self.x = self.bound_width // 2 - self.width // 2
+        self.y = self.bound_height - self.height - self.YGAP
+        self.dx = 0
 
     def model_update(self):
-        pass
+        self.x += self.dx
+        # не дадим игроку выходить за пределы окна
+        if self.x < 0:
+            self.x = 0
+        if self.x > self.bound_width - self.width:
+            self.x = self.bound_width - self.width
 
     def redraw(self, display):
-        pass
+        display.blit(self.img, (self.x, self.y))
 
     def event_process(self, event):
-        pass
+        # нажали клавишу
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.dx = -self.SPEED
+            elif event.key == pygame.K_RIGHT:
+                self.dx = self.SPEED
+        # отпустили клавишу
+        if event.type == pygame.KEYUP:
+            self.dx = 0
+
+    def rect(self):
+        return self.x, self.y, self.width, self.height
+
+
+class Game:
+    def __init__(self, display_size):
+        self.display_size = display_size
+        self.player = Player(display_size)
+
+    def model_update(self):
+        self.player.model_update()
+
+    def redraw(self, display):
+        # заливаем фон
+        w, h = self.display_size
+        display.fill((0, 0, 0), (0, 0, w, h))
+        # рисуем игровые элементы
+        self.player.redraw(display)
+        # запрашиваем обновление экрана
+        pygame.display.update()
+
+    def event_process(self, event):
+        self.player.event_process(event)
 
 
 class Application:

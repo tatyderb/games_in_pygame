856de3318f4e9f3c75e862cd73327915418b8068
tatyderb <tatyderb@gmail.com>
2022-09-24 18:41:12 +0300

    Местоположение ресурсов

---
+++ space_invaders_live/space_invaders_stepik_oop.py
@@ -0,0 +1,20 @@
+
+RSC = {
+    'title': 'Space Invaders',
+    'img': {
+        'background': 'resources/img/background.png',
+        'icon': 'resources/img/ufo.png',
+        'player': 'resources/img/player.png',
+        'enemy': 'resources/img/enemy.png',
+        'bullet': 'resources/img/bullet.png'
+    },
+    'sound': {
+        'background': 'audio/background.wav',
+        'explosion': 'audio/explosion.wav',
+        'laser': 'audio/laser.wav',
+        'endless_big': 'audio/Time-to-Spare-An-Jone.wav'
+    },
+    'font': {
+
+    }
+}

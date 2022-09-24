ff097c1816ad3f371c4b38589644236d80fc73ab
tatyderb <tatyderb@gmail.com>
2022-09-24 21:22:48 +0300

    Bullet - добавили пулю

--- space_invaders_live/space_invaders_stepik_oop.py
+++ space_invaders_live/space_invaders_stepik_oop.py
@@ -60,25 +60,73 @@ class Player:
         return self.x, self.y, self.width, self.height
 
 
+class Bullet:
+    SPEED = 0.5     # скорость пули (абсолютная величина)
+
+    def __init__(self, player_rect):
+        self.img = pygame.image.load(RSC['img']['bullet'])
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+        px, py, pw, ph = player_rect
+        self.x = px + pw // 2 - self.width // 2
+        self.y = py - self.height
+        self.dy = -Bullet.SPEED
+
+    def model_update(self):
+        self.y += self.dy
+
+    def redraw(self, display):
+        display.blit(self.img, (self.x, self.y))
+
+    def rect(self):
+        return self.x, self.y, self.width, self.height
+
+    @staticmethod
+    def fire(event):
+        """ Return True, if press FIRE!!! button. """
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            key = pygame.mouse.get_pressed()
+            if key[0]:
+                return True
+        return False
+
+
+
 class Game:
     def __init__(self, display_size):
         self.display_size = display_size
+        w, h = self.display_size
+        self.bound_rect = pygame.Rect(0, 0, w, h)
         self.player = Player(display_size)
+        self.bullet = None
 
     def model_update(self):
         self.player.model_update()
+        if self.bullet:
+            # если пуля есть, надо ее двигать
+            self.bullet.model_update()
+            if not self.in_bound(self.bullet.rect()):
+                # если пуля вылетела за границы экрана, удаляем ее
+                self.bullet = None
 
     def redraw(self, display):
         # заливаем фон
-        w, h = self.display_size
-        display.fill((0, 0, 0), (0, 0, w, h))
+        display.fill((0, 0, 0), self.bound_rect)
         # рисуем игровые элементы
         self.player.redraw(display)
+        if self.bullet:
+            self.bullet.redraw(display)
         # запрашиваем обновление экрана
         pygame.display.update()
 
     def event_process(self, event):
         self.player.event_process(event)
+        if Bullet.fire(event) and self.bullet is None:
+            self.bullet = Bullet(self.player.rect())
+
+    def in_bound(self, rect):
+        """Возвращает True если rect в границах экрана"""
+        return self.bound_rect.contains(rect)
 
 
 class Application:

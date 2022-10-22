1ef9b07d36eec35e2a987a6315a1044fed441cde
tatyderb <tatyderb@gmail.com>
2022-10-22 15:33:39 +0300

    test list from string

--- uno_live/test_card.py
+++ uno_live/test_card.py
@@ -36,11 +36,22 @@ def test_playable():
 
 
 def test_eq():
-
     t1 = Card('red', 3)
     t2 = Card('red', 3)
-    assert(t1 == t2 and t2 == t1)
+    assert (t1 == t2 and t2 == t1)
 
     c = Card('red', 7)
-    assert(t1 != c and c != t1)
+    assert (t1 != c and c != t1)
 
+
+def test_list_from_str():
+    s = 'y6 r3 g2 b0 b5'
+    expected_list = [
+        Card('yellow', 6),
+        Card('red', 3),
+        Card('green', 2),
+        Card('blue', 0),
+        Card('blue', 5)
+    ]
+    card_list = Card.list_from_str(s)
+    assert (card_list == expected_list)

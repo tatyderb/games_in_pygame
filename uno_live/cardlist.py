0bb2941798b2f0df5e7ca6d85ccd99bd22075417
tatyderb <tatyderb@gmail.com>
2022-10-22 15:34:24 +0300

    Base class CardList

--- uno_live/cardlist.py
+++ uno_live/cardlist.py
@@ -2,44 +2,29 @@ from card import Card
 
 import random
 
-COLORS = ('red', 'green', 'yellow', 'blue')
-NUMBERS = tuple(list(range(0, 10)) + list(range(1, 10)))
 
+class CardList:
 
-class Deck:
-    """Колода карт UNO"""
-    def __init__(self):
-        self.cards = []
+    def __init__(self, cards: list):
+        self.cards = cards
 
-    @staticmethod
-    def generate(colors=COLORS, numbers=NUMBERS):
-        deck = Deck()
-        for color in colors:
-            for number in numbers:
-                deck.add(Card(color, number))
-        return deck
+    def __repr__(self):
+        # h, m, s = '12:05:33'.split(':')
+        # s = ':'.join(['12', '05', '33'])    # s = '12:05:33'
+        s = ' '.join([str(card) for card in self.cards])
+        return s
 
-    @staticmethod
-    def create(card_list):
-        deck = Deck()
-        for card in card_list:
-            deck.add(card)
-        return deck
-
-    @staticmethod
-    def create_from_string(card_list_as_string=''):
-        """По строке вида 'r3 g7 y1' делаю колоду"""
-        deck = Deck()
-        # {'r': 'red', 'g':'green', ...}
-        color_dict = {color[0]: color for color in COLORS}
-        for card in card_list_as_string.split():
-            short_color = card[0]
-            number = int(card[1])
-            deck.add(Card(color_dict[short_color], number))
-        return deck
-
-    def add(self, card):
+    def add(self, card: Card):
         self.cards.append(card)
+
+    def __len__(self):
+        return len(self.cards)
+
+
+class Deck(CardList):
+    """Колода карт UNO"""
+    def __init__(self, cards: list):
+        super().__init__(cards)
 
     def draw(self, size=1):
         """Возвращает или список карт длины size, или одну карту, если size=1.
@@ -52,50 +37,33 @@ class Deck:
             out = out[0]
         return out
 
-    def __repr__(self):
-        # h, m, s = '12:05:33'.split(':')
-        # s = ':'.join(['12', '05', '33'])    # s = '12:05:33'
-        s = ' '.join([str(card) for card in self.cards])
-        return s
-
     def shuffle(self):
         """Перемешивает колоду"""
         random.shuffle(self.cards)
 
 
-class Heap:
+class Heap(CardList):
     """Сброс, верхняя карта (последняя в списке) открыта"""
-    def __init__(self, card):
-        self.cards = [card]
+    def __init__(self, cards):
+        super().__init__(cards)
 
-    def __repr__(self):
+    def __str__(self):
+        """str(heap)"""
         return f'Top: {self.top()}'
 
     def top(self):
         return self.cards[-1]
 
-    def add(self, card):
-        self.cards.append(card)
 
-
-class Hand:
+class Hand(CardList):
     """Рука игрока"""
     def __init__(self, cards):
-        self.cards = cards
-
-    def __repr__(self):
-        return ' '.join([str(card) for card in self.cards])
-
-    def __len__(self):
-        return len(self.cards)
+        super().__init__(cards)
 
     def remove(self, removable_card):
         """Удалить карту с руки removable_card"""
         self.cards.remove(removable_card)
 
-    def playable_list(self, top):
+    def playable_list(self, top) -> list:
         """Возвращает список карт, подходящих для игры на top"""
         return [card for card in self.cards if card.playable(top)]
-
-    def add(self, card):
-        self.cards.append(card)

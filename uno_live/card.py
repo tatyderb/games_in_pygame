0384a6219c61473c4755f0486ad8d4b712940dbc
tatyderb <tatyderb@gmail.com>
2022-10-22 15:32:51 +0300

    all cards + create list from string

--- uno_live/card.py
+++ uno_live/card.py
@@ -1,5 +1,10 @@
+COLORS = ('red', 'green', 'yellow', 'blue')
+NUMBERS = tuple(list(range(0, 10)) + list(range(1, 10)))
+
+
 class Card:
-    def __init__(self, color, number):
+
+    def __init__(self, color: str, number: int|str):
         self.color = color
         self.number = number
 
@@ -14,5 +19,21 @@ class Card:
     def __eq__(self, other):
         """Для работы self == other"""
         return self.color == other.color and self.number == other.number
+
+    @staticmethod
+    def all_cards():
+        return [Card(color, number) for color in COLORS for number in NUMBERS]
+
+    @staticmethod
+    def list_from_str(text='') -> list:
+        """По строке вида 'r3 g7 y1' делаю список карт"""
+        card_list = []
+        # {'r': 'red', 'g':'green', ...}
+        color_dict = {color[0]: color for color in COLORS}
+        for card in text.split():
+            short_color = card[0]
+            number = int(card[1])
+            card_list.append(Card(color_dict[short_color], number))
+        return card_list
 
 

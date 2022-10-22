59f1aa02fbf5c7a7b6fff65856140dc088d5f7cd
tatyderb <tatyderb@gmail.com>
2022-10-22 15:36:23 +0300

    add load/save methods

--- uno_live/uno_application.py
+++ uno_live/uno_application.py
@@ -1,24 +1,46 @@
-from cardlist import Deck, Heap
-from uno_live.player import Player
+import json
+
+from cardlist import Deck, Heap, Hand
+from card import Card
+from player import Player
 
 
 class UnoGame:
     DEFAULT_HAND_SIZE = 7
 
-    def __init__(self, player_names, hand_size=DEFAULT_HAND_SIZE, deck=None):
+    def __init__(self):
+        self.players = None
+        self.player_index = None
+        self.player_size = None
+        self.deck = None
+        self.heap = None
+
+    def __repr__(self):
+        return '\n'.join([
+            repr(self.deck),
+            repr(self.heap),
+            str(self.player_index)
+        ])
+
+
+    @staticmethod
+    def create(player_names: list, hand_size: int = DEFAULT_HAND_SIZE, deck: Deck | None = None):
         """Подготовка к игре:
         * Каждому игроку сдается 7 карт
         * 1 карта открывается и начинает сброс.
         * остальные карты кладутся в колоду в закрытую
         """
+        game = UnoGame()
         if deck is None:
-            self.deck = Deck()
+            game.deck = Deck(Card.all_cards())
         else:
-            self.deck = deck
-        self.players = [Player(name, self.deck.draw(hand_size)) for name in player_names]
-        self.player_index = 0
-        self.player_size = len(self.players)
-        self.heap = Heap(self.deck.draw(1))
+            game.deck = deck
+        game.deck.shuffle()
+        game.players = [Player(name, game.deck.draw(hand_size)) for name in player_names]
+        game.player_index = 0
+        game.player_size = len(game.players)
+        game.heap = Heap([game.deck.draw(1)])
+        return game
 
     def run(self):
         """Сама игра в процессе до конца
@@ -37,13 +59,17 @@ class UnoGame:
         :return: False, если у игрока закончились карты (игра окончена).
         """
         top = self.heap.top()
+        print(self.heap)
         current = self.current_player()
+        print(current)
         card = current.get_payable_card(top)  # убирать карту card из руки
         if card is not None:
             # подходящая карта, кладем в отбой
+            print(f'Play {card}')
             self.heap.add(card)
         else:
             # Если подходящей карты нет, возьми карту из колоды в закрытую.
+            print('Pass! Берем карту из колоды.')
             card = self.deck.draw(1)
             current.add_card(card)
             # * Если она подходит, её можно сразу же сыграть.
@@ -51,6 +77,9 @@ class UnoGame:
             if card is not None:
                 # подходящая карта, кладем в отбой
                 self.heap.add(card)
+                print(f'Play {card}')
+
+        print(current)
 
         if len(current.hand) == 0:
             # текущий игрок сборосил все карты, игра закончена
@@ -71,11 +100,72 @@ class UnoGame:
     def current_player(self):
         return self.players[self.player_index]
 
+    @staticmethod
+    def load(gs: dict):
+        """
+        load game from dict
+        game_state = {
+            'player_index': 0,
+            'players': [
+                {
+                    'name': 'Charly',
+                    'hand': 'r3 r5'
+                },
+                {
+                    'name': 'Bob',
+                    'hand': 'b1 g2'
+                },
 
-# r3 r5 - Alice
+            ],
+            'heap': 'r4 g5 g3',
+            'deck': 'y9 r9 y0 y1'
+        }
+        """
+        game = UnoGame()
+        game.deck = Deck(Card.list_from_str(gs['deck']))
+        game.heap = Heap(Card.list_from_str(gs['heap']))
+        game.player_index = gs['player_index']
+        game.players = [Player(data['name'], Card.list_from_str(data['hand'])) for data in gs['players']]
+        game.player_size = len(game.players)
+        return game
+
+    def save(self):
+        return {
+            'player_index': self.player_index,
+            'players': [{'name': p.name, 'hand': repr(p.hand)} for p in self.players],
+            'heap': repr(self.heap),
+            'deck': repr(self.deck)
+        }
+
+        # r3 r5 - Alice
+
+
 # b1 g2 - Bob
 # g3 - heap
 # y9 - deck
-d = Deck.create_from_string('r3 r5 b1 g2 g3 y9')
-app = UnoGame(['Alice', 'Bob'], hand_size=2, deck=d)
+# d = Deck.create_from_string('r3 r5 b1 g2 g3 y9')
+# app = UnoGame(['Alice', 'Bob'], hand_size=2, deck=d)
+
+
+game_state = {
+    'player_index': 0,
+    'players': [
+        {
+            'name': 'Charly',
+            'hand': 'r3 r5'
+        },
+        {
+            'name': 'Bob',
+            'hand': 'b1 g2'
+        },
+
+    ],
+    'heap': 'r4 g5 g3',
+    'deck': 'y9 r9 y0 y1'
+}
+
+app = UnoGame.create(['Charly', 'Bob', 'Olga', 'Natasha'])
+#app = UnoGame.load(game_state)
+# print(app)
+print(json.dumps(app.save(), indent=4))
 app.run()
